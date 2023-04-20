93b105e9f1497a134d172ab84dd0f94c2cc72bf3
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2023-04-20 14:18:05 +0300

    classes

---
+++ src/main/java/model/Dog.java
@@ -0,0 +1,77 @@
+package model;
+
+import java.util.Objects;
+
+public class Dog {
+
+    private int id;
+
+    private String breed;
+    private int age;
+    private int weight;
+    private String name;
+
+    public Dog(int id, String breed, int age, int weight, String name) {
+        this.id = id;
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public Dog(String breed, int age, int weight, String name) {
+        this.breed = breed;
+        this.age = age;
+        this.weight = weight;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return id == dog.id && age == dog.age && weight == dog.weight && Objects.equals(breed, dog.breed) && Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, breed, age, weight, name);
+    }
+}
