7d926bc4a8e6a82c592e29133b1bb3a6596011e0
roman <roman.pshenichnikov555@gmail.com>
2023-04-25 15:51:05 +0300

    Добавил JavaDoc

--- src/main/java/com/example/animalshelter/services/impl/DogServicesImpl.java
+++ src/main/java/com/example/animalshelter/services/impl/DogServicesImpl.java
@@ -12,10 +12,26 @@ import java.util.Map;
 @Service
 public class DogServicesImpl implements DogServices {
     private final Map<Integer, Dog> dogMap = new HashMap<>();
+
+    /**
+     * Все собаки в БД.
+     * <br>
+     * Используется метод репозитория {@link DogServices#getAll()}
+     * @return Найдены все собаки в БД.
+     */
     @Override
     public Collection<Dog> getAll() {
         return dogMap.values();
     }
+
+    /**
+     * Добавление собак в БД.
+     * <br>
+     * Используется метод репозитория {@link DogServices#addDog(Dog)}
+     * @param dog идентификатор собаки, не может быть null.
+     * @throws InvalidAnimalRequestException если собака под этим id уже существует.
+     * @return Добавляет собаку в БД.
+     */
     @Override
     public Dog addDog(Dog dog) {
         if (dogMap.containsKey(dog.getId())) {
