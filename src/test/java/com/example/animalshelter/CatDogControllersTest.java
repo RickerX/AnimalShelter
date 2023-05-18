dec6e63591da728f3b2d3a9066335a0943dfd92d
Alisa Alexandrova <alisaalexandrova2001@gmail.com>
2023-05-18 13:55:34 +0300

    Тесты

---
+++ src/test/java/com/example/animalshelter/CatDogControllersTest.java
@@ -0,0 +1,53 @@
+package com.example.animalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.json.JSONObject;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class CatDogControllersTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void givenNoCatsInDatabase_whenGetCats_thenEmptyJsonArray() throws Exception{
+        mockMvc.perform(get("/api/cat"))
+                .andExpect(status().isOk())
+                .andExpect( jsonPath("$").isArray())
+                .andExpect( jsonPath("$").isEmpty());
+    }
+
+    @Test
+    void givenNoCatsInDatabase_whenCatAdded_thenItExistsInList() throws Exception{
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("breed","testBreed");
+
+        mockMvc.perform(post("/api/cat/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonObject.toString()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.breed").value("testBreed"));
+
+        mockMvc.perform(get("/api/cat"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].breed").value("testBreed"));
+
+    }
+}
